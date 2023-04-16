58f7b8ac074c90bb85df18709b482e8ff621088d
EdgarBrutyan <115035402+EdgarBrutyan@users.noreply.github.com>
2023-04-16 17:41:42 +0400

    Add files via upload

---
+++ Bishop.h
@@ -0,0 +1,12 @@
+#include "Figure.h"
+#include"Board.h"
+
+#ifndef Bishop_h
+#define Bishop_h
+class Bishop : public Figure {
+public:
+    bool ValidOrNot(const std::pair<int, int>& current, const std::pair<int, int>& new_pos, Board& b) override;
+};
+#endif
+
+
